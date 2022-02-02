558db8aeae85d4826661c0328a44c7f4f95f6067
kingfrankbob <AJWpcc@gmail.com>
2022-02-02 17:29:18 -0600

    aaaaa

--- raspberry_pi/Collision_Text.cpp
+++ raspberry_pi/Collision_Text.cpp
@@ -1,12 +1,8 @@
-/*!
- * @file demo2-hourglass.cpp
- *
- * Slightly more complex example for Adafruit_PixelDust on Raspberry Pi.
- * Has obstacles for particles to move around.
- * REQUIRES rpi-rgb-led-matrix LIBRARY!
- * I2C MUST BE ENABLED using raspi-config!
- *
- */
+/*
+LIBRARY CREATED BY Michael Cragun
+FOR USE ON ADAFRUIT LED MATRIXES
+CREATES BITMAPS AND THE COLLISION FOR THE SAND
+*/
 
 #ifndef ARDUINO // Arduino IDE sometimes aggressively builds subfolders
 //VS CODE SYNC?
@@ -25,15 +21,8 @@ struct RGBLedMatrix *matrix;
 Adafruit_PixelDust *sand = NULL;
 
 
-            
- double A[5][5] = { {0, 0, 1, 0, 0},
-                    {0, 1, 0, 1, 0},
-                    {0, 1, 1, 1, 0},
-                    {0, 1, 0, 1, 0},
-                    {0, 1, 0, 1, 0} };
-                    
 template<int H> 
-void drawBMP(struct LedCanvas *canvas, double a[][H], int L, int x, int o, int R, int G, int B){
+void Collision::drawBMP(struct LedCanvas *canvas, double a[][H], int L, int x, int o, int R, int G, int B){
          for ( int j = 0; j < L; j++, o++) {
            for (int l = 0; l < H; l++){
             if((a[j][l]) == 1){
@@ -44,7 +33,7 @@ void drawBMP(struct LedCanvas *canvas, double a[][H], int L, int x, int o, int R
  }
 
 template<int H>
- void initBMP(double a[][H], int L, int x, int o){
+ void Collision::initBMP(double a[][H], int L, int x, int o){
         for ( int j = 0; j < L; j++, o++) {
            for (int l = 0; l < H; l++){
             if((a[j][l]) == 1){
