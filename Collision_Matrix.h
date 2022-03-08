a20af3126c09b2ded0ef053d0f3bc5da3ea12af0
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2022-03-07 21:47:45 -0600

    Update Collision_Matrix.h

--- Collision_Matrix.h
+++ Collision_Matrix.h
@@ -1,22 +1,31 @@
+/*!
+ * Intro:
+ * Decided to make a function that draws matricies into the screen and gice them collision
+ * ONLY 5x5 MATRIX TESTED SO FAR
+ * Dependencies:Adafruit PixelDust library
+ * Author: Michael Cragun
+ * BSD license, all text here must be included in any redistribution.
+ */
 #ifndef _COLLISION_MATRIX_H_
 #define _COLLISION_MATRIX_H_
 
 
 #ifdef ARDUINO
 #include <Arduino.h>
-#else
+#endif
 
 class Collision_Matrix {
 public:
   
   
   template<int H>
-  void Collision_Matrix::drawMTX(double a[][H], int L, int x, int o, int R, int G, int B);
+  void drawMTX(double a[][H], int L, int x, int o, int R, int G, int B);
   
   
   template<int H>
-  void Collision_Matrix::initMTX(double a[][H], int L, int x, int o);
+  void initMTX(double a[][H], int L, int x, int o);
   
+};
 
-#endif // _COLLISION_MATRIX_H_
+#endif // _COLLISION_MATRIX_H_X_H_
   
